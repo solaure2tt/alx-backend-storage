82debb2c656c92360c7b39082d1ad26ee21ddbd8
solaure2tt <solaure2t@yahoo.fr>
2023-10-12 18:32:23 +0000

    upate task 7 and 11

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -2,9 +2,9 @@
 -- that computes and store the average score for a student
 DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
 DELIMITER $$ ;
-CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INTEGER)
+CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
-	UPDATE users SET average_score = (SELECT AVG(score) AS aver
+	UPDATE users SET average_score = (SELECT AVG(score)
         				 FROM corrections
         				 WHERE corections.user_id=user_id)
 	WHERE id=user_id;
